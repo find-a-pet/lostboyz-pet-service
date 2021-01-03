ea1ea4ceaf6c01e2a5a0e1e7c82947e960905f07
Michael Mkwelele <memkwelele@gmail.com>
2021-01-03 13:25:33 -0800

    Initial commit.

---
+++ src/main/java/com/mtotowamkwe/lostboyzpetservice/LostboyzPetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mtotowamkwe.lostboyzpetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostboyzPetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostboyzPetServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtotowamkwe/lostboyzpetservice/LostboyzPetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtotowamkwe.lostboyzpetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostboyzPetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
